8e3ae3be29ad8e2fdc4b8f8415e7e09fce3be11e
base station <schm1498@umn.edu>
2011-11-12 14:38:55 -0600

    update

---
+++ shirt/build/CMakeFiles/shirt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/shirt.dir/src/ImageUtils.o"
+  "CMakeFiles/shirt.dir/src/Main.o"
+  "../bin/shirt.pdb"
+  "../bin/shirt"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/shirt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)

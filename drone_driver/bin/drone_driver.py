d7d52a0df446bf1155d2807ddee6a61aa0040b13
base station <schm1498@umn.edu>
2011-11-13 19:29:44 -0600

    Update driver for image position and velocity

--- drone_driver/bin/drone_driver.py
+++ drone_driver/bin/drone_driver.py
@@ -6,70 +6,111 @@ from geometry_msgs.msg import TwistStamped
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Empty
 
-import sys, select, termios, tty
+import sys, select, termios, math
 
-prevTwist = TwistStamped()
-twist = TwistStamped()
+# Constants
+X_VEL_SCALAR = 1
+Y_VEL_SCALAR = 1
+Z_VEL_SCALAR = 1
+CONST_SCALAR = [X_VEL_SCALAR, Y_VEL_SCALAR, Z_VEL_SCALAR]
+MIN_DISTANCE = 0
+MAX_SCALAR = 1
 
-def process_image_position (data):
-    global prevTwist
-    global twist
-#    twist.twist.linear.x = 0
-#    twist.twist.linear.y = 0
-#    twist.twist.linear.z = 0
-#    twist.twist.angular.x = 0
-#    twist.twist.angular.y = 0
-#    twist.twist.angular.z = 0
-#    prevTwist.twist.linear.x = 0
-#    prevTwist.twist.linear.y = 0
-#    prevTwist.twist.linear.z = 0
-#    prevTwist.twist.angular.x = 0
-#    prevTwist.twist.angular.y = 0
-#    prevTwist.twist.angular.z = 0
+DirectionLetters = ['x','y','z']
 
+# Global Variables
+MyTwist = TwistStamped()
+
+def CalcScalarXYZ( LineVector ):
+    ReturnScalar = [1,1,1]
+    for i in range(len(DirectionLetters)):
+        Distance = getattr(LineVector, DirectionLetters[i])
+        if math.fabs(Distance) <= MIN_DISTANCE:    
+            ReturnScalar[i] = 0 
+        else:
+            # TODO Apply function to Distance
+            # Cap the Scalar to keep the speed under control
+            ReturnScalar[i] = min(Distance, MAX_SCALAR)
+    return ReturnScalar
+    
+def CalcVelocityFromImagePos( LineVector ):
+    NewVel = Twist().linear
+    Scalar = CalcScalarXYZ( LineVector )
+    for i in range(len(DirectionLetters)):
+        Velocity = CONST_SCALAR[i] * Scalar[i]
+        setattr(NewVel, DirectionLetters[i], Velocity)
+    # Do not allow for vertical motion
+    NewVel.z = 0
+    return NewVel
+
+def CalcAngleFromImagePos( AngleVector ):
+    # TODO Figure out how to compute angular motion
+    NewTwist = Twist().angular
+    for i in range(len(DirectionLetters)):
+        setattr(NewTwist, DirectionLetters[i], 0)
+    return NewTwist
+    
+def ProceImagePosition (data):
+    global MyTwist
 
     pub = rospy.Publisher('cmd_vel', Twist)
     land_pub = rospy.Publisher('/ardrone/land', Empty)
     reset_pub = rospy.Publisher('/ardrone/reset', Empty)
     takeoff_pub = rospy.Publisher('/ardrone/takeoff', Empty)
+    toggleCam = rospy.AddTwoInts('/ardrone/togglecam', toggleCam_service)
+
 #    rospy.loginfo(rospy.get_name()+"I heard %s",data.data)
 
     print "*****"
-    newTwist = TwistStamped()
-    newTwist = data
-    print newTwist.twist.linear
-    key = newTwist.header.frame_id
+    #NewTwist = TwistStamped()
+    NewTwist = data
+    print "In: "
+    print NewTwist.twist.linear
+    key = NewTwist.header.frame_id
     # takeoff and landing
     if key == 'land':
-        rospy.loginfo(rospy.get_name()+"I heard %s",prevTwist.header.frame_id)
+        rospy.loginfo(rospy.get_name()+"I heard %s",NewTwist.header.frame_id)
         land_pub.publish(Empty())
     if key == 'abort':
-        rospy.loginfo(rospy.get_name()+"I heard %s",prevTwist.header.frame_id)
+        rospy.loginfo(rospy.get_name()+"I heard %s",NewTwist.header.frame_id)
         reset_pub.publish(Empty())
     if key == 'takeoff':
-        rospy.loginfo(rospy.get_name()+"I heard %s",prevTwist.header.frame_id)
+        rospy.loginfo(rospy.get_name()+"I heard %s",NewTwist.header.frame_id)
         takeoff_pub.publish(Empty())
     if key == 'hover':
         # TODO Implement hover
-        rospy.loginfo(rospy.get_name()+"I heard %s",prevTwist.header.frame_id)
+        rospy.loginfo(rospy.get_name()+"I heard %s",NewTwist.header.frame_id)
         takeoff_pub.publish(Empty())
-    # compute the difference from the last frame
-    twist.twist.linear.x = prevTwist.twist.linear.x - newTwist.twist.linear.x
-    twist.twist.linear.y = prevTwist.twist.linear.y - newTwist.twist.linear.y
-    twist.twist.linear.z = prevTwist.twist.linear.z - newTwist.twist.linear.z
-    twist.twist.angular.x = prevTwist.twist.angular.x - newTwist.twist.angular.x
-    twist.twist.angular.y = prevTwist.twist.angular.y - newTwist.twist.angular.y
-    twist.twist.angular.z = prevTwist.twist.angular.z - newTwist.twist.angular.z
-    print twist.twist.linear
-    prevTwist = newTwist
-    # Only publish the twist parameters
-    pub.publish(twist.twist)
+    
+    # This input image twist give the delta position and pose for the tag relative 
+    # to the center of the of the image frame.
+    # Therefore the direction of movement is known from the direct input values and
+    # only the magnitude (speed) must be calculated.  
+    # The speed will be calculated based on the previous position and speed vectors
+    # from the previous frames.
+    # If the tag continues to be far from the center of the frame then the speed
+    # will be increased.
+    # If the tag is switching direction in the frame (oscillating) then reduce the
+    # speed to hover over target
+    # The exact function is unknown, so let's start with linear
+    MyTwist.twist.linear = CalcVelocityFromImagePos( NewTwist.twist.linear )
+    MyTwist.twist.angular = CalcAngleFromImagePos( NewTwist.twist.angular )
+    
+    print "Out: "
+    print MyTwist.twist.linear
+    # Only publish the twist parameters to the drone
+    pub.publish(MyTwist.twist)
+    
+    #MyTwist.twist.linear.x = 0
+    #MyTwist.twist.linear.y = 0
+    #MyTwist.twist.linear.z = 0
+    #pub.publish(MyTwist.twist)
 
-def drone_driver():
+def DroneDriver():
     rospy.init_node('drone_driver')
-    rospy.Subscriber("image_pos", TwistStamped, process_image_position )
+    rospy.Subscriber("image_pos", TwistStamped, ProceImagePosition )
     rospy.spin()
 
 if __name__ == '__main__':
-    drone_driver()
-T
+    DroneDriver()
+
